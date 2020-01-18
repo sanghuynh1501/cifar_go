111d18884f5fd3074f30a4056a3fa5b99e87c4ea
sang huynh <sanghuynhnt95@gmail.com>
2020-01-18 18:14:27 +0700

    fix

---
+++ jsbiding/jsbiding.go
@@ -0,0 +1,78 @@
+package jsbiding
+
+import (
+	"image"
+	// "syscall/js"
+)
+
+func GetImage() ([]float64, []float64) {
+	// doc := js.Global().Get("document")
+	// image_element := doc.Call("getElementById", "predict_image")
+	// image_base64 := image_element.Get("src")
+	// coI := strings.Index(image_base64.String(), ",")
+	// raw_image := image_base64.String()[coI+1:]
+
+	// unbased, _ := base64.StdEncoding.DecodeString(string(raw_image))
+	// res := bytes.NewReader(unbased)
+
+	// var rs_image image.Image
+	// switch strings.TrimSuffix(image_base64.String()[5:coI], ";base64") {
+	// case "image/png":
+	// 	log.Println("image/png")
+	// 	pngI, err := png.Decode(res)
+	// 	if err != nil {
+	// 		log.Printf("err ", err)
+	// 	}
+	// 	rs_image = resize.Resize(32, 32, pngI, resize.Bilinear)
+	// case "image/jpeg":
+	// 	log.Println("image/jpeg")
+	// 	jpgI, err := jpeg.Decode(res)
+	// 	if err != nil {
+	// 		log.Printf("err ", err)
+	// 	}
+	// 	rs_image = resize.Resize(32, 32, jpgI, resize.Bilinear)
+	// }
+	// log.Println(rs_image.Bounds())
+	// image_array, _ := getPixels(rs_image)
+	var image_1d []float64
+	for i := 0; i < 32; i++ {
+		for j := 0; j < 32; j++ {
+			image_1d = append(image_1d, 0.5)
+			image_1d = append(image_1d, 0.5)
+			image_1d = append(image_1d, 0.5)
+		}
+	}
+
+	return image_1d, image_1d
+}
+
+// Get the bi-dimensional pixel array
+func getPixels(img image.Image) ([][]Pixel, error) {
+
+	bounds := img.Bounds()
+	width, height := bounds.Max.X, bounds.Max.Y
+
+	var pixels [][]Pixel
+	for y := 0; y < height; y++ {
+		var row []Pixel
+		for x := 0; x < width; x++ {
+			r, g, b, _ := img.At(x, y).RGBA()
+			row = append(row, rgbaToPixel(r, g, b))
+		}
+		pixels = append(pixels, row)
+	}
+
+	return pixels, nil
+}
+
+// img.At(x, y).RGBA() returns four uint32 values; we want a Pixel
+func rgbaToPixel(r uint32, g uint32, b uint32) Pixel {
+	return Pixel{int(r / 257), int(g / 257), int(b / 257)}
+}
+
+// Pixel struct example
+type Pixel struct {
+	R int
+	G int
+	B int
+}
--- main.go
+++ main.go
@@ -1,6 +1,8 @@
 package main
 
 import (
+	"cifar_cnn/jsbiding"
+	"cifar_cnn/weight"
 	"flag"
 	"fmt"
 	"image/jpeg"
@@ -69,24 +71,50 @@ func array_inintial(shape tensor.ConsOpt, input []float64) gorgonia.NodeConsOpt 
 	return gorgonia.WithValue(image)
 }
 
-func newConvNet(g *gorgonia.ExprGraph, w0_object weights, w1_object weights, w2_object weights, w3_object weights, w4_object weights, w5_object weights) *convnet {
+func newConvNet(g *gorgonia.ExprGraph, bs int, w0_object weights, w1_object weights, w2_object weights, w3_object weights, w4_object weights, w5_object weights) *convnet {
+	var b0, b1, b2, b3, b4, b5 *gorgonia.Node
+
 	w0 := gorgonia.NewTensor(g, dt, 4, gorgonia.WithShape(32, 3, 3, 3), gorgonia.WithName("w0"), array_inintial(tensor.WithShape(32, 3, 3, 3), w0_object.weight))
-	b0 := gorgonia.NewTensor(g, dt, 4, gorgonia.WithShape(1, 32, 1, 1), gorgonia.WithName("b0"), array_inintial(tensor.WithShape(1, 32, 1, 1), w0_object.bias))
+	if len(w0_object.bias) > 0 {
+		b0 = gorgonia.NewTensor(g, dt, 4, gorgonia.WithShape(bs, 32, 30, 30), gorgonia.WithName("b0"), array_inintial(tensor.WithShape(bs, 32, 30, 30), w0_object.bias))
+	} else {
+		b0 = gorgonia.NewTensor(g, dt, 4, gorgonia.WithShape(bs, 32, 30, 30), gorgonia.WithName("b0"), gorgonia.WithInit(gorgonia.GlorotN(1.0)))
+	}
 
 	w1 := gorgonia.NewTensor(g, dt, 4, gorgonia.WithShape(32, 32, 3, 3), gorgonia.WithName("w1"), array_inintial(tensor.WithShape(32, 32, 3, 3), w1_object.weight))
-	b1 := gorgonia.NewTensor(g, dt, 4, gorgonia.WithShape(1, 32, 1, 1), gorgonia.WithName("b1"), array_inintial(tensor.WithShape(1, 32, 1, 1), w1_object.bias))
+	if len(w1_object.bias) > 0 {
+		b1 = gorgonia.NewTensor(g, dt, 4, gorgonia.WithShape(bs, 32, 28, 28), gorgonia.WithName("b1"), array_inintial(tensor.WithShape(bs, 32, 28, 28), w1_object.bias))
+	} else {
+		b1 = gorgonia.NewTensor(g, dt, 4, gorgonia.WithShape(bs, 32, 28, 28), gorgonia.WithName("b1"), gorgonia.WithInit(gorgonia.GlorotN(1.0)))
+	}
 
 	w2 := gorgonia.NewTensor(g, dt, 4, gorgonia.WithShape(64, 32, 3, 3), gorgonia.WithName("w2"), array_inintial(tensor.WithShape(64, 32, 3, 3), w2_object.weight))
-	b2 := gorgonia.NewTensor(g, dt, 4, gorgonia.WithShape(1, 64, 1, 1), gorgonia.WithName("b2"), array_inintial(tensor.WithShape(1, 64, 1, 1), w2_object.bias))
+	if len(w2_object.bias) > 0 {
+		b2 = gorgonia.NewTensor(g, dt, 4, gorgonia.WithShape(bs, 64, 12, 12), gorgonia.WithName("b2"), array_inintial(tensor.WithShape(bs, 64, 12, 12), w2_object.bias))
+	} else {
+		b2 = gorgonia.NewTensor(g, dt, 4, gorgonia.WithShape(bs, 64, 12, 12), gorgonia.WithName("b2"), gorgonia.WithInit(gorgonia.GlorotN(1.0)))
+	}
 
 	w3 := gorgonia.NewTensor(g, dt, 4, gorgonia.WithShape(64, 64, 3, 3), gorgonia.WithName("w3"), array_inintial(tensor.WithShape(64, 64, 3, 3), w3_object.weight))
-	b3 := gorgonia.NewTensor(g, dt, 4, gorgonia.WithShape(1, 64, 1, 1), gorgonia.WithName("b3"), array_inintial(tensor.WithShape(1, 64, 1, 1), w3_object.bias))
+	if len(w3_object.bias) > 0 {
+		b3 = gorgonia.NewTensor(g, dt, 4, gorgonia.WithShape(bs, 64, 10, 10), gorgonia.WithName("b3"), array_inintial(tensor.WithShape(bs, 64, 10, 10), w3_object.bias))
+	} else {
+		b3 = gorgonia.NewTensor(g, dt, 4, gorgonia.WithShape(bs, 64, 10, 10), gorgonia.WithName("b3"), gorgonia.WithInit(gorgonia.GlorotN(1.0)))
+	}
 
 	w4 := gorgonia.NewMatrix(g, dt, gorgonia.WithShape(1600, 512), gorgonia.WithName("w4"), array_inintial(tensor.WithShape(1600, 512), w4_object.weight))
-	b4 := gorgonia.NewMatrix(g, dt, gorgonia.WithShape(1, 512), gorgonia.WithName("b4"), array_inintial(tensor.WithShape(1, 512), w4_object.bias))
+	if len(w4_object.bias) > 0 {
+		b4 = gorgonia.NewMatrix(g, dt, gorgonia.WithShape(bs, 512), gorgonia.WithName("b4"), array_inintial(tensor.WithShape(bs, 512), w4_object.bias))
+	} else {
+		b4 = gorgonia.NewMatrix(g, dt, gorgonia.WithShape(bs, 512), gorgonia.WithName("b4"), gorgonia.WithInit(gorgonia.GlorotN(1.0)))
+	}
 
 	w5 := gorgonia.NewMatrix(g, dt, gorgonia.WithShape(512, 10), gorgonia.WithName("w5"), array_inintial(tensor.WithShape(512, 10), w5_object.weight))
-	b5 := gorgonia.NewMatrix(g, dt, gorgonia.WithShape(1, 10), gorgonia.WithName("b5"), array_inintial(tensor.WithShape(1, 10), w5_object.bias))
+	if len(w5_object.bias) > 0 {
+		b5 = gorgonia.NewMatrix(g, dt, gorgonia.WithShape(bs, 10), gorgonia.WithName("b5"), array_inintial(tensor.WithShape(bs, 10), w5_object.bias))
+	} else {
+		b5 = gorgonia.NewMatrix(g, dt, gorgonia.WithShape(bs, 10), gorgonia.WithName("b5"), gorgonia.WithInit(gorgonia.GlorotN(1.0)))
+	}
 
 	return &convnet{
 		g:  g,
@@ -118,7 +146,7 @@ func (m *convnet) learnables() gorgonia.Nodes {
 // This function is particularly verbose for educational reasons. In reality, you'd wrap up the layers within a layer struct type and perform per-layer activations
 func (m *convnet) fwd(x *gorgonia.Node) (err error) {
 	var c0, c1, c2, c3, r3, r4 *gorgonia.Node
-	var b0, b1, b2, b3, b4, b5 *gorgonia.Node
+	// var b0, b1, b2, b3, b4, b5 *gorgonia.Node
 	var a0, a1, a2, a3, a4 *gorgonia.Node
 	var p1, p3 *gorgonia.Node
 	var l1, l3, l4 *gorgonia.Node
@@ -129,10 +157,10 @@ func (m *convnet) fwd(x *gorgonia.Node) (err error) {
 	if c0, err = gorgonia.Conv2d(x, m.w0, tensor.Shape{3, 3}, []int{0, 0}, []int{1, 1}, []int{1, 1}); err != nil {
 		return errors.Wrap(err, "Layer 0 Convolution failed")
 	}
-	if b0, err = gorgonia.Add(c0, m.b0); err != nil {
-		return errors.Wrapf(err, "Unable to multiply l2 and w3")
-	}
-	if a0, err = gorgonia.Rectify(b0); err != nil {
+	// if b0, err = gorgonia.Add(c0, m.b0); err != nil {
+	// 	return errors.Wrapf(err, "Unable to multiply l2 and w3")
+	// }
+	if a0, err = gorgonia.Rectify(c0); err != nil {
 		return errors.Wrap(err, "Layer 0 activation failed")
 	}
 	log.Printf("layer 0 shape %v", a0.Shape())
@@ -143,10 +171,10 @@ func (m *convnet) fwd(x *gorgonia.Node) (err error) {
 	if c1, err = gorgonia.Conv2d(a0, m.w1, tensor.Shape{3, 3}, []int{0, 0}, []int{1, 1}, []int{1, 1}); err != nil {
 		return errors.Wrap(err, "Layer 1 Convolution failed")
 	}
-	if b1, err = gorgonia.Add(c1, m.b1); err != nil {
-		return errors.Wrapf(err, "Unable to multiply l2 and w3")
-	}
-	if a1, err = gorgonia.Rectify(b1); err != nil {
+	// if b1, err = gorgonia.Add(c1, m.b1); err != nil {
+	// 	return errors.Wrapf(err, "Unable to multiply l2 and w3")
+	// }
+	if a1, err = gorgonia.Rectify(c1); err != nil {
 		return errors.Wrap(err, "Layer 1 activation failed")
 	}
 	if p1, err = gorgonia.MaxPool2D(a1, tensor.Shape{2, 2}, []int{0, 0}, []int{2, 2}); err != nil {
@@ -163,10 +191,10 @@ func (m *convnet) fwd(x *gorgonia.Node) (err error) {
 	if c2, err = gorgonia.Conv2d(l1, m.w2, tensor.Shape{3, 3}, []int{0, 0}, []int{1, 1}, []int{1, 1}); err != nil {
 		return errors.Wrap(err, "Layer 2 Convolution failed")
 	}
-	if b2, err = gorgonia.Add(c2, m.b2); err != nil {
-		return errors.Wrapf(err, "Unable to multiply l2 and w3")
-	}
-	if a2, err = gorgonia.Rectify(b2); err != nil {
+	// if b2, err = gorgonia.Add(c2, m.b2); err != nil {
+	// 	return errors.Wrapf(err, "Unable to multiply l2 and w3")
+	// }
+	if a2, err = gorgonia.Rectify(c2); err != nil {
 		return errors.Wrap(err, "Layer 2 activation failed")
 	}
 	log.Printf("layer 2 shape %v", a2.Shape())
@@ -177,10 +205,10 @@ func (m *convnet) fwd(x *gorgonia.Node) (err error) {
 	if c3, err = gorgonia.Conv2d(a2, m.w3, tensor.Shape{3, 3}, []int{0, 0}, []int{1, 1}, []int{1, 1}); err != nil {
 		return errors.Wrap(err, "Layer 3 Convolution failed")
 	}
-	if b3, err = gorgonia.Add(c3, m.b3); err != nil {
-		return errors.Wrapf(err, "Unable to multiply l2 and w3")
-	}
-	if a3, err = gorgonia.Rectify(b3); err != nil {
+	// if b3, err = gorgonia.Add(c3, m.b3); err != nil {
+	// 	return errors.Wrapf(err, "Unable to multiply l2 and w3")
+	// }
+	if a3, err = gorgonia.Rectify(c3); err != nil {
 		return errors.Wrap(err, "Layer 3 activation failed")
 	}
 	if p3, err = gorgonia.MaxPool2D(a3, tensor.Shape{2, 2}, []int{0, 0}, []int{2, 2}); err != nil {
@@ -204,10 +232,10 @@ func (m *convnet) fwd(x *gorgonia.Node) (err error) {
 	if r4, err = gorgonia.Mul(r3, m.w4); err != nil {
 		return errors.Wrapf(err, "Unable to multiply l2 and w3")
 	}
-	if b4, err = gorgonia.Add(r4, m.b4); err != nil {
-		return errors.Wrapf(err, "Unable to multiply l2 and w3")
-	}
-	if a4, err = gorgonia.Rectify(b4); err != nil {
+	// if b4, err = gorgonia.Add(r4, m.b4); err != nil {
+	// 	return errors.Wrapf(err, "Unable to multiply l2 and w3")
+	// }
+	if a4, err = gorgonia.Rectify(r4); err != nil {
 		return errors.Wrapf(err, "Unable to activate fc")
 	}
 	if l4, err = gorgonia.Dropout(a4, m.d4); err != nil {
@@ -220,10 +248,10 @@ func (m *convnet) fwd(x *gorgonia.Node) (err error) {
 	if out, err = gorgonia.Mul(l4, m.w5); err != nil {
 		return errors.Wrapf(err, "Unable to multiply l3 and w4")
 	}
-	if b5, err = gorgonia.Add(out, m.b5); err != nil {
-		return errors.Wrapf(err, "Unable to multiply l2 and w3")
-	}
-	m.out, err = gorgonia.SoftMax(b5)
+	// if b5, err = gorgonia.Add(out, m.b5); err != nil {
+	// 	return errors.Wrapf(err, "Unable to multiply l2 and w3")
+	// }
+	m.out, err = gorgonia.SoftMax(out)
 	gorgonia.Read(m.out, &m.predVal)
 	log.Printf("layer 5 shape %v", out.Shape())
 
@@ -270,203 +298,64 @@ func main() {
 	flag.Parse()
 	parseDtype()
 	rand.Seed(1337)
-
 	g := gorgonia.NewGraph()
-	xT := tensor.New(tensor.WithShape(10, 32, 30, 30), tensor.WithBacking(tensor.Range(tensor.Float64, 0, 288000)))
-	yT := tensor.New(tensor.WithShape(1, 32, 1, 1), tensor.WithBacking(tensor.Range(tensor.Float64, 0, 32)))
 
-	x := gorgonia.NewTensor(g, gorgonia.Float64, 4, gorgonia.WithShape(10, 32, 30, 30), gorgonia.WithValue(xT), gorgonia.WithName("x"))
-	y := gorgonia.NewTensor(g, gorgonia.Float64, 4, gorgonia.WithShape(1, 32, 1, 1), gorgonia.WithValue(yT), gorgonia.WithName("y"))
-	a, b, _ := gorgonia.Broadcast(x, y, gorgonia.NewBroadcastPattern(nil, []byte{9}))
-	log.Println(a.Shape())
-	log.Println(b.Shape())
-	// z, _ := gorgonia.Add(a, b)
-	// log.Println(z)
+	bs := 2
+	var err error
+	w0, b0, w1, b1, w2, b2, w3, b3, w4, b4, w5, b5 := weight.Load()
+	image1, image2 := jsbiding.GetImage()
+	xValue1 := gorgonia.NewTensor(g, dt, 4, gorgonia.WithShape(1, 3, 32, 32), gorgonia.WithName("xValue1"), array_inintial(tensor.WithShape(1, 3, 32, 32), image1))
+	xValue2 := gorgonia.NewTensor(g, dt, 4, gorgonia.WithShape(1, 3, 32, 32), gorgonia.WithName("xValue2"), array_inintial(tensor.WithShape(1, 3, 32, 32), image2))
+	xValue, err := gorgonia.Concat(0, xValue1, xValue2)
 
-	// // intercept Ctrl+C
-	// sigChan := make(chan os.Signal, 1)
-	// signal.Notify(sigChan, syscall.SIGINT, syscall.SIGTERM)
-	// doneChan := make(chan bool, 1)
+	w0_object := weights{w0, b0}
+	w1_object := weights{w1, b1}
+	w2_object := weights{w2, b2}
+	w3_object := weights{w3, b3}
+	w4_object := weights{w4, b4}
+	w5_object := weights{w5, b5}
 
-	// var inputs, targets tensor.Tensor
-	// var err error
-
-	// // go func() {
-	// // 	log.Println(http.ListenAndServe("localhost:6060", nil))
-	// // }()
-
-	// trainOn := *dataset
-	// if inputs, targets, err = cifar.Load(trainOn, loc); err != nil {
-	// 	log.Fatal(err)
-	// }
-
-	// numExamples := inputs.Shape()[0]
-	// bs := *batchsize
-
-	// if err := inputs.Reshape(numExamples, 3, 32, 32); err != nil {
-	// 	log.Fatal(err)
-	// }
-	// g := gorgonia.NewGraph()
 	// x := gorgonia.NewTensor(g, dt, 4, gorgonia.WithShape(bs, 3, 32, 32), gorgonia.WithName("x"))
-	// y := gorgonia.NewMatrix(g, dt, gorgonia.WithShape(bs, 10), gorgonia.WithName("y"))
-	// w0, b0, w1, b1, w2, b2, w3, b3, w4, b4, w5, b5 := weight.Load()
+	m := newConvNet(g, bs, w0_object, w1_object, w2_object, w3_object, w4_object, w5_object)
 
-	// w0_object := weights{w0, b0}
-	// w1_object := weights{w1, b1}
-	// w2_object := weights{w2, b2}
-	// w3_object := weights{w3, b3}
-	// w4_object := weights{w4, b4}
-	// w5_object := weights{w5, b5}
+	if err = m.fwd(xValue); err != nil {
+		log.Fatalf("%+v", err)
+	}
+	mv := gorgonia.NewTapeMachine(g)
+	mv.RunAll()
+	arrayOutput := m.predVal.Data().([]float64)
+	yOutput := tensor.New(tensor.WithShape(bs, 10), tensor.WithBacking(arrayOutput))
+	for i := 0; i < yOutput.Shape()[0]; i++ {
+		predRowT, _ := yOutput.Slice(sli{i, i + 1})
+		predRow := predRowT.Data().([]float64)
+		var rowGuess int
+		var predRowHigh float64
 
-	// m := newConvNet(g, w0_object, w1_object, w2_object, w3_object, w4_object, w5_object)
-	// if err = m.fwd(x); err != nil {
-	// 	log.Fatalf("%+v", err)
-	// }
-	// if err = m.fwd(x); err != nil {
-	// 	log.Fatalf("%+v", err)
-	// }
-	// losses := gorgonia.Must(gorgonia.HadamardProd(gorgonia.Must(gorgonia.Log(m.out)), y))
-	// cost := gorgonia.Must(gorgonia.Sum(losses))
-	// cost = gorgonia.Must(gorgonia.Neg(cost))
+		// guess result
+		for k := 0; k < bs; k++ {
+			if k == 0 {
+				rowGuess = 0
+				predRowHigh = predRow[k]
+			} else if predRow[k] > predRowHigh {
+				rowGuess = k
+				predRowHigh = predRow[k]
+			}
+		}
+		log.Println("rowGuess ", rowGuess)
+	}
 
-	// // we wanna track costs
-	// var costVal gorgonia.Value
-	// gorgonia.Read(cost, &costVal)
-
-	// if _, err = gorgonia.Grad(cost, m.learnables()...); err != nil {
-	// 	log.Fatal(err)
-	// }
-
-	// // debug
-	// ioutil.WriteFile("fullGraph.dot", []byte(g.ToDot()), 0644)
-
-	// prog, locMap, _ := gorgonia.Compile(g)
-	// log.Printf("%v", prog)
-
-	// vm := gorgonia.NewTapeMachine(g, gorgonia.WithPrecompiled(prog, locMap), gorgonia.BindDualValues(m.learnables()...))
+	// // prog, locMap, _ := gorgonia.Compile(g)
+	// vm := gorgonia.NewTapeMachine(g)
 	// defer vm.Close()
-
-	// var profiling bool
-	// if *cpuprofile != "" {
-	// 	f, err := os.Create(*cpuprofile)
-	// 	if err != nil {
-	// 		log.Fatal(err)
-	// 	}
-	// 	profiling = true
-	// 	pprof.StartCPUProfile(f)
-	// 	defer pprof.StopCPUProfile()
+	// gorgonia.Let(x, xValue)
+	// log.Print("xValue.Shape() ", xValue.Shape())
+	// if err = vm.RunAll(); err != nil {
+	// 	log.Fatalf("Failed at epoch test: %v", err)
 	// }
-	// go cleanup(sigChan, doneChan, profiling)
-
-	// batches := numExamples / bs
-	// log.Printf("Batches %d", batches)
-	// bar := pb.New(batches)
-	// bar.SetRefreshRate(time.Second)
-	// bar.SetMaxWidth(80)
-
-	// // import test data and run more loops
-	// if inputs, targets, err = cifar.Load("test", loc); err != nil {
-	// 	log.Fatal(err)
-	// }
-
-	// batches = inputs.Shape()[0] / bs
-	// bar = pb.New(batches)
-	// bar.SetRefreshRate(time.Second)
-	// bar.SetMaxWidth(80)
-
-	// var testActual, testPred []int
-
-	// total := 0.0
-	// total_true := 0.0
-
-	// for i := 0; i < 1; i++ {
-	// 	bar.Prefix(fmt.Sprintf("Epoch Test"))
-	// 	bar.Set(0)
-	// 	bar.Start()
-	// 	for b := 0; b < batches; b++ {
-	// 		start := b * bs
-	// 		end := start + bs
-	// 		if start >= numExamples {
-	// 			break
-	// 		}
-	// 		if end > numExamples {
-	// 			end = numExamples
-	// 		}
-
-	// 		var xVal, yVal tensor.Tensor
-	// 		if xVal, err = inputs.Slice(sli{start, end}); err != nil {
-	// 			log.Fatal("Unable to slice x")
-	// 		}
-
-	// 		if yVal, err = targets.Slice(sli{start, end}); err != nil {
-	// 			log.Fatal("Unable to slice y")
-	// 		}
-	// 		if err = xVal.(*tensor.Dense).Reshape(bs, 3, 32, 32); err != nil {
-	// 			log.Fatalf("Unable to reshape %v", err)
-	// 		}
-
-	// 		gorgonia.Let(x, xVal)
-	// 		gorgonia.Let(y, yVal)
-	// 		if err = vm.RunAll(); err != nil {
-	// 			log.Fatalf("Failed at epoch test: %v", err)
-	// 		}
-
-	// 		arrayOutput := m.predVal.Data().([]float64)
-	// 		yOutput := tensor.New(tensor.WithShape(bs, 10), tensor.WithBacking(arrayOutput))
-
-	// 		for j := 0; j < yVal.Shape()[0]; j++ {
-
-	// 			// get label
-	// 			yRowT, _ := yVal.Slice(sli{j, j + 1})
-	// 			yRow := yRowT.Data().([]float64)
-	// 			var rowLabel int
-	// 			var yRowHigh float64
-
-	// 			for k := 0; k < 10; k++ {
-	// 				if k == 0 {
-	// 					rowLabel = 0
-	// 					yRowHigh = yRow[k]
-	// 				} else if yRow[k] > yRowHigh {
-	// 					rowLabel = k
-	// 					yRowHigh = yRow[k]
-	// 				}
-	// 			}
-
-	// 			// get prediction
-	// 			predRowT, _ := yOutput.Slice(sli{j, j + 1})
-	// 			predRow := predRowT.Data().([]float64)
-	// 			var rowGuess int
-	// 			var predRowHigh float64
-
-	// 			// guess result
-	// 			for k := 0; k < 10; k++ {
-	// 				if k == 0 {
-	// 					rowGuess = 0
-	// 					predRowHigh = predRow[k]
-	// 				} else if predRow[k] > predRowHigh {
-	// 					rowGuess = k
-	// 					predRowHigh = predRow[k]
-	// 				}
-	// 			}
-
-	// 			if rowGuess == rowLabel {
-	// 				total_true += 1
-	// 			}
-	// 			total += 1
-
-	// 			testActual = append(testActual, rowLabel)
-	// 			testPred = append(testPred, rowGuess)
-	// 		}
-
-	// 		vm.Reset()
-	// 		bar.Increment()
-	// 	}
-	// 	log.Printf("Epoch Test | cost %v", costVal)
-	// 	log.Printf("Epoch Test | accurancy %v", (total_true/total)*100)
-
-	// 	printIntSlice("testActual.txt", testActual)
-	// 	printIntSlice("testPred.txt", testPred)
-	// }
+	// log.Printf("m.out ", m.out.Value())
+	// arrayOutput := m.predVal.Data().([]float64)
+	// yOutput := tensor.New(tensor.WithShape(bs, 10), tensor.WithBacking(arrayOutput))
+	// print("yOutput ", yOutput.Data().([]float64))
 }
 
 func cleanup(sigChan chan os.Signal, doneChan chan bool, profiling bool) {
